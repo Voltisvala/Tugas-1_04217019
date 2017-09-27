a20661dd28180a7b654c3d916e42aad60dc9a053
Voltisvala <31975975+Voltisvala@users.noreply.github.com>
2017-09-27 20:15:00 +0700

    Add files via upload

---
+++ tugas-1_04217019.py
@@ -0,0 +1,11 @@
+nama="firman rizaldy"
+nim="04217019"
+umur=18
+x=(umur + 5)
+
+print ("Nama : " + nama)
+print ("Nim : " + nim)
+print "umur"
+print (umur)
+print "umur setelah lima tahun"
+print (x)
